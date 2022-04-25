6894307014238aa01c2d11450b68690f913621ad
Althaf <alufabzak47@example.com>
2022-04-26 03:58:31 +0530

    althaf

--- my_app/views.py
+++ my_app/views.py
@@ -24,3 +24,35 @@ def admin_worker_profile_view(request):
     
 def admin_workers_details(request):
     return render(request,'admin_workers_details.html')
+    
+def admin_user_activity(request):
+    return render(request,'admin_user_activity.html')
+
+def admin_workers_register(request):
+    return render(request,'admin_workers_register.html')
+
+def admin_feedback_cards(request):
+    return render(request,'admin_feedback_cards.html')
+
+def admin_workers_feedbacks(request):
+    return render(request,'admin_workers_feedbacks.html')
+
+def admin_users_feedbacks(request):
+    return render(request,'admin_users_feedbacks.html')
+
+############################user################################
+
+def user_dashboard(request):
+    return render(request,'user_dashboard.html')
+
+def user_search_worker(request):
+    return render(request,'user_search_worker.html')
+
+def user_worker_profile(request):
+    return render(request,'user_worker_profile.html')
+
+def user_worker_reports(request):
+    return render(request,'user_worker_reports.html')
+
+def user_history_list(request):
+    return render(request,'user_history_list.html')
--- webscraping/urls.py
+++ webscraping/urls.py
@@ -1,4 +1,4 @@
-"""webscraping URL Configuration
+"""project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -28,4 +28,18 @@ urlpatterns = [
     path('admin_user_profile_view/', views.admin_user_profile_view, name='admin_user_profile_view'),
     path('admin_worker_profile_view/', views.admin_worker_profile_view, name='admin_worker_profile_view'),
     path('admin_workers_details/', views.admin_workers_details, name='admin_workers_details'),
+    path('admin_workers_register/', views.admin_workers_register, name='admin_workers_register'),
+    path('admin_user_activity/', views.admin_user_activity, name='admin_user_activity'),
+    path('admin_feedback_cards/', views.admin_feedback_cards, name='admin_feedback_cards'),
+    path('admin_workers_feedbacks/', views.admin_workers_feedbacks, name='admin_workers_feedbacks'),
+    path('admin_users_feedbacks/', views.admin_users_feedbacks, name='admin_users_feedbacks'),
+
+    #############################user##########################
+
+    path('user_dashboard/', views.user_dashboard, name='user_dashboard'),
+    path('user_search_worker/', views.user_search_worker, name='user_search_worker'),
+    path('user_worker_profile/', views.user_worker_profile, name='user_worker_profile'),
+    path('user_worker_reports/', views.user_worker_reports, name='user_worker_reports'),
+    path('user_history_list/', views.user_history_list, name='user_history_list'),
+
 ]
